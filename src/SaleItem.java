22b49bbea8c601e48b4793dd502bbc2e8e51445c
jharknes <jharknes@oswego.edu>
2017-02-24 14:26:59 -0500

    Add files via upload

---
+++ src/SaleItem.java
@@ -0,0 +1,15 @@
+/**
+ * Created by joshu on 2/24/2017.
+ */
+public class SaleItem {
+    private double price;
+    private String productName;
+    private String productBrand;
+    private int upc;
+    private int quantity;
+    private String category;
+
+    SaleItem(double p) {
+        price = p;
+    }
+}
---
+++ src/Shelf.java
@@ -0,0 +1,10 @@
+/**
+ * Created by joshu on 2/24/2017.
+ */
+public class Shelf {
+    private String name;
+
+    Shelf(String n) {
+        name = n;
+    }
+}
