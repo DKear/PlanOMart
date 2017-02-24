3432fdfe6aadb88fd0a29d13ba6411b299409ffd
PlanOMart <planomart380@gmail.com>
2017-02-23 20:36:36 -0500

    Fixed

---
+++ src/Store.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+/**
+ * Created by Jamie on 2/23/2017.
+ */
+public class Store {
+    private String storeName;
+
+
+    Store(String n){
+        storeName = n;
+    }
+    public String getStoreName(){
+        return storeName;
+    }
+
+
+}
