c161c91cca881b8a92ace86677d20ab0c9ccc2a1
jharknes <jharknes@oswego.edu>
2017-03-02 09:33:41 -0500

    Fixed the sale price methods but...
    Now the null pointer exceptions aren't being thrown. for some reason, although I create bread with a null name, it assigns it the name of white bread.

---
+++ SaleItemTest.java
@@ -0,0 +1,193 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by joshu on 2/24/2017.
+ */
+public class SaleItemTest {
+    @Test
+    public void getPriceTest() {
+        SaleItem bread = new SaleItem(1.39, "White Bread", "Wonder Bread", "bread");
+        Assertions.assertEquals(1.39, bread.getPrice());
+    }
+
+    @Test
+    public void tooLowOfPriceTest() {
+        SaleItem bread = new SaleItem(-1.39, "White Classic", "Wonder Bread", "bread");
+        Assertions.assertFalse(bread.validateItem(bread));
+    }
+
+    @Test
+    public void getNameTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        Assertions.assertTrue(bread.getName().equals("White Classic"));
+    }
+
+    @Test
+    public void nullNameTest() {
+        SaleItem bread;
+        boolean test = false;
+        try {
+            bread = new SaleItem(1.39, null, "Wonder Bread", "bread");
+        } catch (NullPointerException e) {
+            test = true;
+            Assertions.assertTrue(test);
+        }
+        Assertions.assertTrue(test);
+    }
+
+    @Test
+    public void emptyStringNameTest() {
+        SaleItem bread = new SaleItem(1.39, "", "Wonder Bread", "bread");
+        Assertions.assertFalse(bread.validateItem(bread));
+    }
+
+    @Test
+    public void getBrandTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        Assertions.assertTrue(bread.getBrand().equals("Wonder Bread"));
+    }
+
+    @Test
+    public void nullBrandTest() {
+        SaleItem bread;
+        boolean test = false;
+        try {
+            bread = new SaleItem(1.39, "White Classic", null, "bread");
+        } catch (NullPointerException e) {
+            test = true;
+            Assertions.assertTrue(test);
+        }
+        Assertions.assertTrue(test);
+    }
+
+    @Test
+    public void emptyStringBrandTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "", "bread");
+        Assertions.assertFalse(bread.validateItem(bread));
+    }
+
+    @Test
+    public void doesNotHaveTagTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        Assertions.assertFalse(bread.hasTags());
+    }
+
+    @Test
+    public void hasTagsTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        String tag = "sliced";
+        bread.addTag(tag);
+        Assertions.assertTrue(bread.hasTags());
+    }
+
+    @Test
+    public void getTagsTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        String tag = "sliced";
+        bread.addTag(tag);
+        Assertions.assertTrue(bread.getTags()[0].equals("sliced"));
+    }
+
+    @Test
+    public void noTagsToGetTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        boolean test = false;
+        try {
+            bread.getTags();
+        } catch (ArrayStoreException e) {
+            test = true;
+            Assertions.assertTrue(test);
+        }
+        Assertions.assertTrue(test);
+    }
+
+    @Test
+    public void tagRemovedFromItemPositive() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        String tag = "sliced";
+        bread.addTag(tag);
+        Assertions.assertTrue(bread.removeTag(tag));
+    }
+
+    @Test
+    public void tagRemovedFromShelfNegativeTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        String tag = "sliced";
+        Assertions.assertFalse(bread.removeTag(tag));
+    }
+
+    @Test
+    public void getSalePriceTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        bread.setSalePercentage(10);
+        Assertions.assertEquals(1.25, bread.getSalePrice());
+    }
+    /*@Test
+    public void itemSalePriceTooLowTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        boolean test = false;
+        try {
+            bread.setSalePercentage(100); //anything less than -100 would make the sale price higher than the actual
+
+        } catch (IllegalArgumentException e) {
+            test = true;
+            Assertions.assertTrue(test);
+        }
+        Assertions.assertTrue(test);
+    }
+    */
+    @Test
+    public void itemSalePriceTooHighTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        boolean test = false;
+        try {
+            bread.setSalePercentage(0);
+        } catch (IllegalArgumentException e) {
+            test = true;
+            Assertions.assertTrue(test);
+        }
+        Assertions.assertTrue(test);
+    }
+    @Test
+    public void getSaleDescriptionTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        bread.setSaleDescription("Buy One Get One");
+        Assertions.assertTrue(bread.getSaleDescription().equals("Buy One Get One"));
+    }
+
+    @Test
+    public void saleDescriptionIsNullTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        boolean test = false;
+        try {
+            bread.setSaleDescription(null);
+        } catch (NullPointerException e) {
+            test = true;
+            Assertions.assertTrue(test);
+        }
+        Assertions.assertTrue(test);
+    }
+
+    @Test
+    public void saleDescriptionEmptyStringTest() {
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        boolean test = false;
+        try {
+            bread.setSaleDescription("");
+        } catch (IllegalArgumentException e) {
+            test = true;
+            Assertions.assertTrue(test);
+        }
+        Assertions.assertTrue(test);
+    }
+    @Test
+    public void setGetShelfTest(){
+        Shelf shelf = new Shelf(10, 10);
+        SaleItem bread = new SaleItem(1.39, "White Classic", "Wonder Bread", "bread");
+        shelf.addItem(bread);
+        bread.setShelf(shelf);
+        Assertions.assertTrue(bread.getShelf().equals(shelf) & shelf.getItemsOnShelf()[0].equals(bread));
+    }
+
+}
---
+++ Shelf.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+/**
+ * Created by joshu on 2/24/2017.
+ */
+public class Shelf {
+    private int rowNum;
+    private double availableSize;
+    private ArrayList<SaleItem> products;
+    private double maxShelfSize = 1440; // square inches based on 2.5 feet deep and 4 feet across
+    private Rack rack;
+
+
+    public Shelf(int n, int s) {
+        if(n >= 0 && s >= 0 && s < maxShelfSize) {
+            rowNum = n;
+            availableSize = s;
+            products = new ArrayList<SaleItem>();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+    public int getRowNum(){
+        return rowNum;
+    }
+    public int getNumOfItemsOnShelf(){
+        return products.size();
+    }
+    public double getAvailableSize() {
+        return availableSize;
+    }
+    public double getShelfMaxSize() {
+        return maxShelfSize;
+    }
+    public SaleItem[] getItemsOnShelf() {
+        if (!products.isEmpty()) {
+            SaleItem[] itemsOnShelf = new SaleItem[products.size()];
+            itemsOnShelf = products.toArray(itemsOnShelf);
+            return itemsOnShelf;
+        } else {
+            throw new ArrayStoreException();
+        }
+    }
+    public boolean addItem(SaleItem i) {
+
+        return products.add(i);
+
+        /*if (availableSize < i.getTotalAreaConsumed()) {
+            return false;
+        } else {
+            availableSize -= i.getTotalAreaConsumed();
+            return products.add(i);
+        }*/
+    }
+    public boolean hasItems() {
+        if (products.size() == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public boolean removeItems(SaleItem i) {
+           return products.remove(i);
+    }
+
+    public void setRack(Rack r){
+        rack = r;
+    }
+
+    public Rack getRack(){
+        return rack;
+    }
+}
