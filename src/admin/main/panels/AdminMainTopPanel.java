d56a6fa66f217eeba69523b6bb934220ac31ce25
jharknes <jharknes@oswego.edu>
2017-04-22 19:11:57 -0400

    Merge branches 'DylanBranch' and 'master' of https://github.com/DKear/PlanOMart
    # Conflicts:
    #	.idea/workspace.xml
    #	out/production/PlanOMart/GUI$1.class
    #	out/production/PlanOMart/GUI.class
    #	out/production/PlanOMart/GUIAdminEdit.class
    #	out/production/PlanOMart/store/locations/Store.class
    #	src/GUI.java
    #	src/admin/main/panels/AdminMainBottomPanel.java
    #	src/admin/main/panels/AdminMainTopPanel.java

--- src/admin/main/panels/AdminMainTopPanel.java
+++ src/admin/main/panels/AdminMainTopPanel.java
@@ -5,8 +5,7 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 
 public class AdminMainTopPanel extends JPanel {
     private JLabel title;
@@ -16,16 +15,13 @@ public class AdminMainTopPanel extends JPanel {
 
         setBorder(new BevelBorder(BevelBorder.RAISED));
         title = new JLabel("Plan-O-Mart");
-//        title.setPreferredSize(new Dimension(400, 600));
         title.setFont(new Font("Arial", Font.BOLD, 64));
 
         adminLabel = new JLabel("Administrator Account");
-//        adminLabel.setPreferredSize(new Dimension(200, 600));
         adminLabel.setFont(new Font("Arials", Font.PLAIN, 32));
 
         setLayout(new MigLayout("", "[]", "[]push[]"));
         add(title, "align left");
         add(adminLabel, "align center");
     }
-    private JTextField searchField;
 }
